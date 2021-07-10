0d7957ccb433cfda3b502be8c8db797012f59fcf
Maryam Awesu <ajayiemmanuel630@gmail.com>
2021-07-10 07:47:31 +0100

    netlify form set up for index page

--- index.js
+++ index.js
@@ -9,25 +9,38 @@ const email = getId("email");
 const names = getId("name");
 let getName, setName, nameArr, lastName;
 
+let formGet = e => {
+  e.preventDefault();
+  emailPattern = /\S+@\S+\.\S+/;
+  nameArr = names.value.split(" "); //split the input name into an array
+  lastName = nameArr[nameArr.length - 1]; //extract the last name
+  localStorage.setItem("name", lastName); //store the last inputed name in a local storage
 
-let formGet = (e) => {
-    e.preventDefault();
-    emailPattern = /\S+@\S+\.\S+/;
-    nameArr = names.value.split(" ");//split the input name into an array
-    lastName = nameArr[nameArr.length - 1];//extract the last name
-    localStorage.setItem('name', lastName);//store the last inputed name in a local storage
+  //custom form validation
+  if (
+    emailPattern.test(email.value) &&
+    email.value !== "" &&
+    !names.value.includes("+")
+  ) {
+    email.setCustomValidity("");
+    names.setCustomValidity("");
 
-    //custom form validation
-    if (emailPattern.test(email.value) && email.value !== "" && !names.value.includes('+')) {
-        email.setCustomValidity("");
-        names.setCustomValidity('')
-    } else if (names.value.includes('+')) {
-        names.setCustomValidity('pls input correct format')
-    } else {
-        names.setCustomValidity('')
-        email.setCustomValidity("pls include '@' in your email");
-    }
-}
+    // setup netlify form
+    let formData = new FormData(form);
+    fetch("/", {
+      method: "POST",
+      headers: {"Content-Type": "application/x-www-form-urlencoded"},
+      body: new URLSearchParams(formData).toString(),
+    })
+      .then(() => console.log("Form successfully submitted"))
+      .catch(error => alert(error));
+  } else if (names.value.includes("+")) {
+    names.setCustomValidity("pls input correct format");
+  } else {
+    names.setCustomValidity("");
+    email.setCustomValidity("pls include '@' in your email");
+  }
+};
 
-hiName.innerHTML = `HI ${localStorage.getItem('name')}!`;
-form.addEventListener('submit', formGet);
+hiName.innerHTML = `HI ${localStorage.getItem("name")}!`;
+form.addEventListener("submit", formGet);
